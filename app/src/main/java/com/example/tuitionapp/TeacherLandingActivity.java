32726e031c12f948a10e2a96d31d277e6cf09de5
Towsif13 <towsifc@gmail.com>
2020-02-01 19:55:30 +0600

    Landing activities created

---
+++ app/src/main/java/com/example/tuitionapp/TeacherLandingActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuitionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_landing);
+    }
+}
