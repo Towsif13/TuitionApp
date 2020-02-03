9382d332eab8dba0b5914e6784a551a478c56da4
Youme01 <samiya.kabir@northsouth.edu>
2020-02-03 22:25:22 +0600

    Fragments created and Top navigation is added

---
+++ app/src/main/java/com/example/tuitionapp/ChatsFragment_Student.java
@@ -0,0 +1,31 @@
+package com.example.tuitionapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ChatsFragment_Student extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats_fragment__student, container, false);
+    }
+
+//blaaaajjjjlll
+
+
+//lllll
+}
