8c2fa9397788552ab256a6cc7ab172a09e0def2d
Alvi0017 <alvi0019@gmail.com>
2020-02-23 20:15:11 +0600

    might be error ar ffixed

--- app/src/main/java/com/example/tuitionapp/PublicStudentProfileActivity.java
+++ app/src/main/java/com/example/tuitionapp/PublicStudentProfileActivity.java
@@ -82,55 +82,70 @@ public class PublicStudentProfileActivity extends AppCompatActivity {
         studentMedium = findViewById(R.id.studentMedium);
         studentClass = findViewById(R.id.studentClass);
         send_req = findViewById(R.id.st_send_req);
+        try {
+            mAuth = FirebaseAuth.getInstance();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            myref = mFirebaseDatabase.getReference();
+            sturef = mFirebaseDatabase.getReference().child("AcceptStudent");
+            final FirebaseUser user = mAuth.getCurrentUser();
+            try {
+                uid = user.getUid();
+                Intent intent = getIntent();
+                receiverUserId = getIntent().getExtras().getString("userid");
+            }catch (Exception e){
+                Toast.makeText(this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
 
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        myref = mFirebaseDatabase.getReference();
-        sturef = mFirebaseDatabase.getReference().child("AcceptStudent");
-        final FirebaseUser user = mAuth.getCurrentUser();
-        uid = user.getUid();
-        Intent intent = getIntent();
-        receiverUserId = getIntent().getExtras().getString("userid");
 
 
-        myref.child("Users").child("Student").child(receiverUserId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            myref.child("Users").child("Student").child(receiverUserId).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                if(dataSnapshot.exists()) {
+                    if(dataSnapshot.exists()) {
+                        try {
+                            String fname = dataSnapshot.child("FirstName").getValue().toString();
+                            String lname = dataSnapshot.child("LastName").getValue().toString();
+                            String name = fname + " " + lname;
+                            String email = user.getEmail();
+                            String phone = dataSnapshot.child("Phone").getValue().toString();
+                            String region = dataSnapshot.child("Region").getValue().toString();
+                            String address = dataSnapshot.child("Address").getValue().toString();
+                            String dob = dataSnapshot.child("Birthday").getValue().toString();
+                            String gender = dataSnapshot.child("Gender").getValue().toString();
+                            String medium = dataSnapshot.child("Medium").getValue().toString();
+                            String classs = dataSnapshot.child("Class").getValue().toString();
 
-                    String fname = dataSnapshot.child("FirstName").getValue().toString();
-                    String lname = dataSnapshot.child("LastName").getValue().toString();
-                    String name = fname + " " + lname;
-                    String email = user.getEmail();
-                    String phone = dataSnapshot.child("Phone").getValue().toString();
-                    String region = dataSnapshot.child("Region").getValue().toString();
-                    String address = dataSnapshot.child("Address").getValue().toString();
-                    String dob = dataSnapshot.child("Birthday").getValue().toString();
-                    String gender = dataSnapshot.child("Gender").getValue().toString();
-                    String medium = dataSnapshot.child("Medium").getValue().toString();
-                    String classs = dataSnapshot.child("Class").getValue().toString();
+                            studentName.setText(name);
+                            studentEmail.setText(email);
+                            studentPhone.setText(phone);
+                            studentRegion.setText(region);
+                            studentAddress.setText(address);
+                            studentDOB.setText(dob);
+                            studentGender.setText(gender);
+                            studentMedium.setText(medium);
+                            studentClass.setText(classs);
 
-                    studentName.setText(name);
-                    studentEmail.setText(email);
-                    studentPhone.setText(phone);
-                    studentRegion.setText(region);
-                    studentAddress.setText(address);
-                    studentDOB.setText(dob);
-                    studentGender.setText(gender);
-                    studentMedium.setText(medium);
-                    studentClass.setText(classs);
+                            MaintenanceOfButtons();
 
-                    MaintenanceOfButtons();
+                        }catch (Exception e){
+                            Toast.makeText(getApplication(), "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
                 }
 
-            }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
-            }
-        });
 
 
         add_btn_student_profile.setOnClickListener(new View.OnClickListener() {
@@ -244,17 +259,22 @@ public class PublicStudentProfileActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.hasChild(receiverUserId)) {
+                    try {
+                        String request_type = dataSnapshot.child(receiverUserId).child("request_type").getValue().toString();
+                        if (request_type.equals("sent")) {
+                            Current_state = "request_sent";
+                            send_txt.setTag("Cancel Request");
+                            send_req.setImageResource(R.drawable.ic_plus_one_black_24dp);
+                        }else if(request_type.equals("received")){
+                            Current_state = "request_received";
+                            send_txt.setTag("Accept Request");
 
-                    String request_type = dataSnapshot.child(receiverUserId).child("request_type").getValue().toString();
-                    if (request_type.equals("sent")) {
-                        Current_state = "request_sent";
-                        send_txt.setTag("Cancel Request");
-                        send_req.setImageResource(R.drawable.ic_plus_one_black_24dp);
-                    }else if(request_type.equals("received")){
-                        Current_state = "request_received";
-                        send_txt.setTag("Accept Request");
+                        }
 
+                    }catch (Exception e){
+                        Toast.makeText(PublicStudentProfileActivity.this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
+
                 }
             }
 
